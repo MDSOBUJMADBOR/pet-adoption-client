e54abeafe89c277a5b3a7a8f15c684cac7a0e9b0
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-05-25 23:31:23 +0600

    edit navbar

--- src/app/user/dashboard/my-requests/page.jsx
+++ src/app/user/dashboard/my-requests/page.jsx
@@ -3,11 +3,12 @@ import UserTable from '@/components/UserTable';
 import React from 'react';
 
 const MyRequestsPage = async() => { 
+
    
           return (
                     <div>
 <h2 className='text-4xl font-bold text-green-500'>My Adoption Requests</h2>
-
+<p>sobuj</p>
 
 <UserTable></UserTable>
 
